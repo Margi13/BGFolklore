0e2a2a1fe2ff288717552f3e7b1d15ead7aa89d7
Margarita <margarita.petrova.kn@gmail.com>
2021-06-21 15:49:02 +0300

    Добавен контролер за Календара с предстоящи и всички събития/места. Добавено View към него и добавени връзки от Layout, Home/Index към Calendar/Index.

---
+++ BGFolklore.Web/Controllers/CalendarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGFolklore.Web.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UpcomingEvents()
+        {
+            return View();
+        }
+        public IActionResult AllPlaces()
+        {
+            return View();
+        }
+    }
+}
