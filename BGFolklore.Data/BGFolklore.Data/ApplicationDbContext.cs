fb49d001d8f1474bb374cf22df9911dbe8f8a8cd
Margarita <margarita.petrova.kn@gmail.com>
2021-06-21 23:48:23 +0300

    Създаден клас, който оформя модела на един event и е създадена миграция към базата данни по този модел.

--- BGFolklore.Data/BGFolklore.Data/ApplicationDbContext.cs
+++ BGFolklore.Data/BGFolklore.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BGFolklore.Data.Models.Calendar;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace BGFolklore.Data
             : base(options)
         {
         }
+
+        public DbSet<PublicEvent> PublicEvents { get; set; }
     }
 }
---
+++ BGFolklore.Data/BGFolklore.Models/Calendar/PublicEvent.cs
@@ -0,0 +1,50 @@
+﻿using BGFolklore.Common.Nomenclatures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace BGFolklore.Data.Models.Calendar
+{
+    public class PublicEvent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime InsertDateTime { get; set; }
+
+        public DateTime? UpdateDateTime { get; set; }
+
+        [Required]
+        public DateTime EventDateTime { get; set; }
+
+        public int OccuringDays { get; set; }
+
+        [Required]
+        public PlaceType PlaceType { get; set; }
+
+        [Required]
+        public int IntendedFor { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Town { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Description { get; set; }
+
+        public float Rating { get; set; }
+    }
+}
