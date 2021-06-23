940ded2ac6df66ca17eebdb2cb0fdf56142f9662
Margarita <margarita.petrova.kn@gmail.com>
2021-06-23 20:59:13 +0300

    Създаден е ресурсен файл, който съдържа преводите за AttendeeType. Оправена е логиката и външният вид за създаване на ново събитие. Оптимизация на Layout. Оптимизация със section Scripts/CSS във view-тата

--- BGFolklore.Common/Nomenclatures/PlaceType.cs
+++ BGFolklore.Common/Nomenclatures/PlaceType.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BGFolklore.Common.Nomenclatures
 {
     public enum PlaceType
     {
+        [Display(Name ="Отворено събитие")]
         Open = 1,
+
+        [Display(Name = "Танцов състав")]
         Ensemble = 2,
+
+        [Display(Name = "Заведение")]
         Restaurant = 3
     }
 }
--- BGFolklore.Web/Controllers/CalendarController.cs
+++ BGFolklore.Web/Controllers/CalendarController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BGFolklore.Common.Nomenclatures;
+using BGFolklore.Models.Calendar.BindingModels;
+using BGFolklore.Models.Calendar.ViewModels;
+using BGFolklore.Services.Public.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +16,83 @@ namespace BGFolklore.Web.Controllers
 {
     public class CalendarController : Controller
     {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly ICalendarService calendarService;
+        private readonly IStringLocalizer<CalendarController> localizer;
+        private readonly IMapper mapper;
+
+        public CalendarController(IWebHostEnvironment webHostEnvironment,
+            ICalendarService calendarService,
+            IStringLocalizer<CalendarController> localizer,
+            IMapper mapper)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.calendarService = calendarService;
+            this.localizer = localizer;
+            this.mapper = mapper;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult UpcomingEvents()
         {
-            return View();
+            IList<UpcomingEventViewModel> viewModelList = calendarService.GetUpcomingEvents();
+            return View(viewModelList);
         }
         public IActionResult RecurringEvents()
         {
+            //IList<RecurringEventViewModel> viewModelList = calendarService.GetRecurringEvents();
+            //return View(viewModelList);
             return View();
+        }
+        [HttpGet]
+        public IActionResult AddEvent()
+        {
+            AddEventViewModel viewModel = GetAddEventViewModel();
+
+            return View(viewModel);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventBindingModel addEventBindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                calendarService.SaveAddEvent(addEventBindingModel);
+                return RedirectToAction("UpcomingEvents");
+            }
+            else
+            {
+                var addEventViewModel = this.mapper.Map<AddEventViewModel>(addEventBindingModel);
+
+                addEventViewModel.IntendedFor = new List<SelectListItem>();
+                GetAttendeeType(addEventViewModel);
+                //Как да ги отбележа тези, които са били отбелязани?
+                return View(addEventViewModel);
+            }
+        }
+
+        private AddEventViewModel GetAddEventViewModel()
+        {
+            var viewModel = new AddEventViewModel();
+            viewModel.IntendedFor = new List<SelectListItem>();
+            GetAttendeeType(viewModel);
+
+            return viewModel;
+        }
+
+        private void GetAttendeeType(AddEventViewModel viewModel)
+        {
+            foreach (var type in Enum.GetValues(typeof(AttendeeType)))
+            {
+                var selectListItem = new SelectListItem();
+                selectListItem.Value = ((int)type).ToString();
+                selectListItem.Text = localizer[$"AttendeeType{type}"];
+                viewModel.IntendedFor.Add(selectListItem);
+            }
         }
     }
 }
