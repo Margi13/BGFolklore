2543d6bdcc1383a59dd809e26b024e2b71a6c8f3
Margarita <margarita.petrova.kn@gmail.com>
2021-09-12 19:49:22 +0300

    Изборът на дата за двата вида събития е направен по един и същи начин с едно input поле, вместо с две (едно за дата и час, друго само за час). Направен е map от FilterBindingModel към FilterViewModel Направен е отделен клас в Web.Common, в който ще се слагат всички методи, които не са обвързани с базата данни и се използват от CalendarController.

---
+++ BGFolklore.Web/Common/ViewModelHelper.cs
@@ -0,0 +1,179 @@
+﻿using AutoMapper;
+using BGFolklore.Common.Nomenclatures;
+using BGFolklore.Models.Calendar.BindingModels;
+using BGFolklore.Models.Calendar.ViewModels;
+using BGFolklore.Web.Controllers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGFolklore.Web.Common
+{
+    public class ViewModelHelper
+    {
+        private readonly IStringLocalizer<CalendarController> localizer;
+        private readonly IMapper mapper;
+
+        public ViewModelHelper(IStringLocalizer<CalendarController> localizer, IMapper mapper)
+        {
+            this.localizer = localizer;
+            this.mapper = mapper;
+        }
+
+        public AddEventViewModel CreateAddEventViewModel()
+        {
+            var viewModel = new AddEventViewModel();
+
+            viewModel.IntendedFor = GetAttendeeType();
+            viewModel.OccuringDays = GetOccuringDays();
+
+            return viewModel;
+        }
+        public AddEventViewModel CreateAddEventViewModel(AddEventBindingModel addEventBindingModel)
+        {
+            var viewModel = this.mapper.Map<AddEventViewModel>(addEventBindingModel);
+
+            viewModel.IntendedFor = new List<SelectListItem>();
+            GetSelectedAttendeeType(viewModel, addEventBindingModel);
+
+            viewModel.OccuringDays = new List<SelectListItem>();
+            GetSelectedOccuringDays(viewModel, addEventBindingModel);
+
+            return viewModel;
+        }
+
+        public FilterViewModel CreateFilterViewModel()
+        {
+            var viewModel = new FilterViewModel();
+
+            viewModel.IntendedFor = GetAttendeeType();
+            viewModel.OccuringDays = GetOccuringDays();
+
+            return viewModel;
+        }
+        public FilterViewModel CreateFilterViewModel(FilterBindingModel filterBindingModel)
+        {
+            var viewModel = this.mapper.Map<FilterViewModel>(filterBindingModel);
+
+            viewModel.IntendedFor = new List<SelectListItem>();
+            GetSelectedAttendeeType(viewModel, filterBindingModel);
+
+            viewModel.OccuringDays = new List<SelectListItem>();
+            GetSelectedOccuringDays(viewModel, filterBindingModel);
+
+            return viewModel;
+        }
+
+        public List<SelectListItem> GetOccuringDays()
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+            foreach (var dayName in Enum.GetValues(typeof(DaysOfWeek)))
+            {
+                var selectListItem = new SelectListItem();
+                selectListItem.Value = ((int)dayName).ToString();
+                selectListItem.Text = localizer[$"Day{dayName}"];
+                result.Add(selectListItem);
+            }
+            return result;
+        }
+
+        public void GetSelectedOccuringDays(AddEventViewModel viewModel, AddEventBindingModel bindingModel)
+        {
+            viewModel.OccuringDays = GetOccuringDays();
+
+            if (bindingModel.OccuringDays != null)
+            {
+                foreach (var dayName in viewModel.OccuringDays)
+                {
+                    dayName.Selected = false;
+
+                    foreach (var selectedDay in bindingModel.OccuringDays)
+                    {
+                        if (dayName.Value.CompareTo(selectedDay.ToString()) == 0)
+                        {
+                            dayName.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+        public void GetSelectedOccuringDays(FilterViewModel viewModel, FilterBindingModel bindingModel)
+        {
+            viewModel.OccuringDays = GetOccuringDays();
+
+            if (bindingModel.OccuringDays != null)
+            {
+                foreach (var dayName in viewModel.OccuringDays)
+                {
+                    dayName.Selected = false;
+
+                    foreach (var selectedDay in bindingModel.OccuringDays)
+                    {
+                        if (dayName.Value.CompareTo(selectedDay.ToString()) == 0)
+                        {
+                            dayName.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<SelectListItem> GetAttendeeType()
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+            foreach (var type in Enum.GetValues(typeof(AttendeeType)))
+            {
+                var selectListItem = new SelectListItem();
+                selectListItem.Value = ((int)type).ToString();
+                selectListItem.Text = localizer[$"AttendeeType{type}"];
+                result.Add(selectListItem);
+            }
+            return result;
+        }
+
+        public void GetSelectedAttendeeType(AddEventViewModel viewModel, AddEventBindingModel bindingModel)
+        {
+            viewModel.IntendedFor = GetAttendeeType();
+
+            if (bindingModel.IntendedFor != null)
+            {
+                foreach (var attendee in viewModel.IntendedFor)
+                {
+                    attendee.Selected = false;
+
+                    foreach (var selectedAttendee in bindingModel.IntendedFor)
+                    {
+                        if (attendee.Value.CompareTo(selectedAttendee.ToString()) == 0)
+                        {
+                            attendee.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+        public void GetSelectedAttendeeType(FilterViewModel viewModel, FilterBindingModel bindingModel)
+        {
+            viewModel.IntendedFor = GetAttendeeType();
+
+            if (bindingModel.IntendedFor != null)
+            {
+                foreach (var attendee in viewModel.IntendedFor)
+                {
+                    attendee.Selected = false;
+
+                    foreach (var selectedAttendee in bindingModel.IntendedFor)
+                    {
+                        if (attendee.Value.CompareTo(selectedAttendee.ToString()) == 0)
+                        {
+                            attendee.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
--- BGFolklore.Web/Mapping/AutoMapperProfile.cs
+++ BGFolklore.Web/Mapping/AutoMapperProfile.cs
@@ -15,28 +15,28 @@ namespace BGFolklore.Web.Mapping
     {
         public AutoMapperProfile()
         {
+            //Image Maps
             this.CreateMap<ImageViewModel, Image>().ReverseMap();
             this.CreateMap<Area, AreaImagesViewModel>();
 
-
+            //Video Maps
             this.CreateMap<VideoViewModel, Video>().ReverseMap();
             this.CreateMap<Area, AreaVideosViewModel>();
 
+            //PublicEvent Maps
             this.CreateMap<PublicEvent, UpcomingEventViewModel>();
-
             this.CreateMap<PublicEvent, RecurringEventViewModel>();
-
             this.CreateMap<PublicEvent, EventViewModel>();
+            this.CreateMap<PublicEvent, AddEventBindingModel>()
+                .ForMember(dest => dest.IntendedFor, opt => opt.Ignore())
+                .ForMember(dest => dest.OccuringDays, opt => opt.Ignore());
 
+            //ViewModel Maps
             this.CreateMap<AddEventBindingModel, PublicEvent>()
                 .ForMember(dest => dest.IntendedFor, opt => opt.Ignore())
                 .ForMember(dest => dest.OccuringDays, opt => opt.Ignore())
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.Town, opt => opt.Ignore());
-
-            this.CreateMap<PublicEvent, AddEventBindingModel>()
-                .ForMember(dest => dest.IntendedFor, opt => opt.Ignore())
-                .ForMember(dest => dest.OccuringDays, opt => opt.Ignore());
 
             this.CreateMap<AddEventBindingModel, AddEventViewModel>()
                 .ForMember(dest => dest.IntendedFor, opt => opt.Ignore())
@@ -46,18 +46,25 @@ namespace BGFolklore.Web.Mapping
                 .ForMember(dest => dest.IntendedFor, opt => opt.Ignore())
                 .ForMember(dest => dest.OccuringDays, opt => opt.Ignore());
 
+            this.CreateMap<FilterBindingModel, FilterViewModel>()
+                .ForMember(dest => dest.IntendedFor, opt => opt.Ignore())
+                .ForMember(dest => dest.OccuringDays, opt => opt.Ignore());
+
+            //Feedback Maps
             this.CreateMap<FeedbackBindingModel, Feedback>()
                 .ForMember(dest => dest.Event, opt => opt.Ignore())
                 .ForMember(dest => dest.Status, opt => opt.Ignore())
                 .ForMember(dest => dest.Owner, opt => opt.Ignore());
 
+            this.CreateMap<FeedbackBindingModel, FeedbackViewModel>();
+
+            this.CreateMap<Feedback, FeedbackViewModel>();
+
+            //Rating Maps
             this.CreateMap<RatingBindingModel, Rating>()
                 .ForMember(dest => dest.Event, opt => opt.Ignore())
                 .ForMember(dest => dest.Owner, opt => opt.Ignore());
 
-            this.CreateMap<FeedbackBindingModel, FeedbackViewModel>();
-
-            this.CreateMap<Feedback, FeedbackViewModel>();
         }
     }
 }
