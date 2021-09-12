8866e37d0e10040340634fd0bcafb2c4011f0c91
Margarita <margarita.petrova.kn@gmail.com>
2021-09-12 20:33:32 +0300

    Взети са всички областни градове и са направени да се изписват във формата за филтрирането

--- BGFolklore.Web/Common/Towns.cs
+++ BGFolklore.Web/Common/Towns.cs
@@ -13,10 +13,24 @@ namespace BGFolklore.Web.Common
     {
 
         public static ICollection<Town> AllTowns;
+        public static ICollection<Town> AllAreas;
 
         public static void GetTowns(ITownsService townsService)
         {
             AllTowns = townsService.GetAllTowns();
+            AllAreas = townsService.GetAllAreas();
+        }
+        public static ICollection<Town> GetTownsByAreaId(int areaId)
+        {
+            ICollection<Town> towns = new List<Town>();
+            foreach (var town in AllTowns)
+            {
+                if(town.AreaId == areaId)
+                {
+                    towns.Add(town);
+                }
+            }
+            return towns;
         }
 
     }
