7168da842160e9866105fe3c56c01af54cc43fc3
Margarita <margarita.petrova.kn@gmail.com>
2021-09-12 19:45:10 +0300

    Добавен е view model за филтрирането и всички пропъртитата в binding model са направени nullable

--- BGFolklore.Models/Calendar/BindingModels/FilterBindingModel.cs
+++ BGFolklore.Models/Calendar/BindingModels/FilterBindingModel.cs
@@ -1,6 +1,7 @@
 ﻿using BGFolklore.Common.Nomenclatures;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,25 @@ namespace BGFolklore.Models.Calendar.BindingModels
 {
     public class FilterBindingModel
     {
-        public Guid TownId { get; set; }
-        public Guid AreaId { get; set; }
-        public Guid OwnerId { get; set; }
-        public PlaceType PlaceType { get; set; }
+        public Guid? OwnerId { get; set; }
+        [Required]
+        public bool IsRecurring { get; set; }
 
-        public DateTime BeforeDate { get; set; }
-        public DateTime AfterDate { get; set; }
+        public Guid? AreaId { get; set; }
+
+        public Guid? TownId { get; set; }
+
+        public PlaceType? PlaceType { get; set; }
+
+        public DateTime? AfterDate { get; set; }
+
+        public DateTime? BeforeDate { get; set; }
+
+
+        public int[] IntendedFor { get; set; }
+
         public int DurationInDays { get; set; }
-        public int OccuringDays { get; set; }
-        public int IntendedFor { get; set; }
+
+        public int[] OccuringDays { get; set; }
     }
 }
---
+++ BGFolklore.Models/Calendar/ViewModels/FilterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using BGFolklore.Common.Nomenclatures;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGFolklore.Models.Calendar.ViewModels
+{
+    public class FilterViewModel
+    {
+        public Guid? OwnerId { get; set; }
+        [Required]
+        public bool IsRecurring { get; set; }
+
+        [Display(Name = "Област")]
+        public Guid? AreaId { get; set; }
+
+        [Display(Name = "Град")]
+        public Guid? TownId { get; set; }
+
+        [Display(Name = "Тип на мястото")]
+        public PlaceType? PlaceType { get; set; }
+
+        [Display(Name = "След дата")]
+        public DateTime? AfterDate { get; set; }
+
+        [Display(Name = "Преди дата")]
+        public DateTime? BeforeDate { get; set; }
+
+        [Display(Name = "Препоръчано за")]
+        public List<SelectListItem> IntendedFor { get; set; }
+
+        [Display(Name = "Продължителност в дни")]
+        public int DurationInDays { get; set; }
+
+        [Display(Name = "Дни от седмицата")]
+        public List<SelectListItem> OccuringDays { get; set; }
+    }
+}
