02b875e599188349a252926ed03959165225a3ce
Margarita <margarita.petrova.kn@gmail.com>
2021-09-16 15:43:07 +0300

    Добавени са константи за ролите.

---
+++ BGFolklore.Common/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGFolklore.Common
+{
+    public class Constants
+    {
+        public const string AdminRoleName = "Administrator";
+        public const string ModeratorRoleName = "Moderator";
+        public const string UserRoleName = "User";
+        public const string OrgRoleName = "Organization";
+    }
+}
