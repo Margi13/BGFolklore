d563701e609905392958fcf4240e133eb3050588
Stefan <steff.kn@abv.bg>
2021-09-22 00:01:00 +0300

    Добавени атрибути към модела

--- BGFolklore.Models/Admin/ViewModels/ManageFeedbackViewModel.cs
+++ BGFolklore.Models/Admin/ViewModels/ManageFeedbackViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,24 @@ namespace BGFolklore.Models.Admin.ViewModels
 {
     public class ManageFeedbackViewModel
     {
+        [Key]
         public Guid Id { get; set; }
 
         public DateTime CreateDateTime { get; set; }
-
+        [Required]
         public string OwnerId { get; set; }
 
         public string OwnerUserName { get; set; }
-
+        [Required]
         public Guid EventId { get; set; }
         public string EventName { get; set; }
 
         public int StatusId { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително, ако искате да изпратите сигнал за грешна информация!")]
+        [MaxLength(256, ErrorMessage = "Описанието трябва да е по-малко от 256 символа.")]
+        [MinLength(3, ErrorMessage = "Описанието трябва да е от поне 3 символа.")]
+        [Display(Name = "Опишете намерената грешна информация")]
         public string Description { get; set; }
     }
 }
